3259d66c394c6677652d60d8595bda59a5f149fc
Auguste Quenum <cooviquenum8@gmail.com>
2024-10-24 21:24:04 +0200

    ted

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,124 +1,100 @@
 <script setup>
-import { onMounted, ref } from 'vue';
-import { RouterLink, RouterView } from 'vue-router'
-
-const allPays = ref([]);
-const dataPays = ref([]);
-
-const pays = () => {
-  return fetch("https://restcountries.com/v3.1/all")
-    .then(res => res.json())
-    .then((listPays) => {
-      allPays.value = listPays
-      for (let index = 0; index < allPays.value.length; index++) {
-        if (index <= 99) {
-          dataPays.value.push({
-            flags: allPays.value[index].flags.png,
-            name: allPays.value[index].name.common,
-            shitOption: shuffleArray([allPays.value[Math.floor(Math.random() * 100)].name.common,
-                        allPays.value[Math.floor(Math.random() * 100)].name.common,
-                        allPays.value[index].name.common])
-          })
-        }
-
-      }
-
-      console.log(dataPays.value);
-
-    })
-    .catch(console.error());
-};
-onMounted(() => {
-  pays();
-
-})
-function reinitialiseObj() {
-  obj.value = {
-    id: "",
-    drapeau: "",
-    nom: "",
-  }
-};
-
-let objListPays = ref({
-  id: "",
-  drapeau: "",
-  nom: "",
-})
-let obj = ref({
-  id: "",
-  drapeau: "",
-  nom: "",
-});
-
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1)); // Choisir un index aléatoire
-        // Échanger les éléments
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-}
 
 
-function addListPays(array, length) {
-  for (let i = 0; i < length; i++) {
-    objListPays.nom = array[i].name.common,
-      objListPays.drapeau = array[i].flags.png
 
-    allPays.value.push(objListPays);
-  }
-}
-
-function nextQuestion() {
-  if (currentIndex.value < dataPays.value.length - 1) {
-    currentIndex.value++;
-  } else {
-    currentIndex.value = 0; // Recommencer si c'est la dernière question
-  }
-}
 </script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Streack</RouterLink>
-    </nav>
-    <div v-if="dataPays.length > 0">
-      <img :src="dataPays[currentIndex].flags" alt="Drapeau" />
-      <h2>{{ dataPays[currentIndex].name }}</h2>
-      <div>
-        <p>Choisissez l'option correcte :</p>
+<div class="container">
+  <head>
+    <div class="barre">
+      <nav>
         <ul>
-          <li v-for="(option, index) in dataPays[currentIndex].shitOption" :key="index">
-            {{ option }}
-          </li>
+          <li id="retour"> <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z"/></svg></li>
+          <li class="score"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg> 00</li>
+          <li class="score">00</li>
+          <li><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M520-200v-560h240v560H520Zm-320 0v-560h240v560H200Zm400-80h80v-400h-80v400Zm-320 0h80v-400h-80v400Zm0-400v400-400Zm320 0v400-400Z"/></svg></li>
+          <li><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M560-131v-82q90-26 145-100t55-168q0-94-55-168T560-749v-82q124 28 202 125.5T840-481q0 127-78 224.5T560-131ZM120-360v-240h160l200-200v640L280-360H120Zm440 40v-322q47 22 73.5 66t26.5 96q0 51-26.5 94.5T560-320ZM400-606l-86 86H200v80h114l86 86v-252ZM300-480Z"/></svg></li>
         </ul>
-      </div>
-      <button @click="nextQuestion">Next</button>
+      </nav>
     </div>
-  </header>
+    <div class="drap">
+      <img src="/Flag_of_France.png" alt="" width="200px">
+        <div class="loader"></div>
+    </div>
+    <div class="select">
+      <p>Pologne</p>
+      <p>France</p>
+      <p>Italie</p>
+    </div>
+  </head>
 
-  <RouterView />
+  <h1>Auguste</h1>
+</div>
+  
 </template>
 
-<!-- </script>
+<style scoped>
+/* HTML: <div class="loader"></div> */
 
-<template>
-  <header>
-    <nav>
-      <RouterLink to="/">Streack</RouterLink>
-    </nav>
-    <div v-for="(x, index) in dataPays">
-      {{ x.flags }} ::: {{ x.name }}
-    </div>
-    <div v-for="(x, index) in allPays">
-      {{ index }}: "{{ x.flags?.png }}"
-    </div>
+.loader {
+  /* width: 120px; */
+  height: 22px;
+  border-radius: 20px;
+  color: #4b827b;
+  border: 2px solid;
+  position: relative;
+}
+.loader::after {
+  content: "";
+  position: absolute;
+  margin: 2px;
+  inset: 0 100% 0 0;
+  border-radius: inherit;
+  background: currentColor;
+  animation: l6 5s infinite;
+}
+@keyframes l6 {
+    0% {inset:0}
+}
+.container {
+  text-align: center;
+}
+.score{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 15px;
+  width: 50px;
+}
+nav ul {
+  display: flex;
+  justify-content: center;
+}
+ul li {
+  display: flex;
+  justify-content: space-between;
+}
+li {
+  border: 1px solid;
+  border-radius: 30px;
+  width: 30px;
+  margin: 10px;
+  background-color: white;
+  
+}
+p{
+  border: 1px solid;
+  border-radius: 15px;
+  background-color: white;
+  margin: 20px 39% 0px ;
+  padding: 15px;
+}
 
-  </header>
-
-  <RouterView />
-</template> -->
-
-<style scoped></style>
+#retour{
+    cursor: pointer;
+}
+.loader {
+  margin: 20px 39% 0px ;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -6,12 +6,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/streack',
       name: 'streack',
       component: Streack,
     },
     {
-      path: '/home',
+      path: '/',
       name: 'about',
       component: Home
     },

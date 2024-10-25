fe19175727b92cdf3b45d32758f2cbce02165066
KpoglaEvaelle <evaellekpogla@gmail.com>
2024-10-25 09:48:20 +0200

    home-view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,4 @@
 <script setup>
-<<<<<<< HEAD
-<<<<<<< HEAD
 import LoaderComponent from '../components/LoaderComponent.vue';
 import MaisonComponent from '../components/MaisonComponent.vue';
 import SecondeComponent from '../components/GameComponent.vue';
@@ -24,19 +22,3 @@ affiche()
 </template>
 <style scoped>
 </style>
-=======
-=======
->>>>>>> 8e4a5a1afe304824ea801498ca183f3822677d09
-
-</script>
-
-<template>
-<div>
-<h1>je suis le composant home</h1>
-</div>
-
-</template>
-<<<<<<< HEAD
->>>>>>> 8e4a5a1afe304824ea801498ca183f3822677d09
-=======
->>>>>>> 8e4a5a1afe304824ea801498ca183f3822677d09
